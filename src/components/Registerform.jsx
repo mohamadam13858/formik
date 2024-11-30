36358fc2250613caacbc6e5684638d0edb1f8458
mohamad <mh711748@gmail.com>
2024-11-30 18:34:07 +0330

    validationSchema-Yup-getFieldProps

--- src/components/Registerform.jsx
+++ src/components/Registerform.jsx
@@ -1,36 +1,57 @@
 
 import React from 'react';
 import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 const Registerform = () => {
-    const formik = useFormik({
-        initialValues:{
-            name: 'qasem',
-            email: '',
-            password: '',
-        },
-        onSubmit: values=>{
-            console.log(values);
-        },
-        validate: values=>{
 
-            let errors = {}
-            if (!values.name){
-                errors.name = "لطفا این قسمت را پر کنید"
-            }
-            if (!values.email){
-                errors.email = "لطفا این قسمت را پر کنید"
-            }else if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email)){
-                errors.email = "لطفا قالب ایمیل را درست وارد کنید مثال : mh711748@gmail.com"
-            }
-            if (!values.password){
-                errors.password = "لطفا این قیمت را پر کنید"
-            }
-            return errors;
+
+    const initialValues = {
+        name: 'qasem',
+        email: '',
+        password: '',
+    }
+
+    const onSubmit = values => {
+        console.log(values);
+
+    }
+    const validate = values => {
+
+        let errors = {}
+        if (!values.name) {
+            errors.name = "لطفا این قسمت را پر کنید"
         }
+        if (!values.email) {
+            errors.email = "لطفا این قسمت را پر کنید"
+        } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email)) {
+            errors.email = "لطفا قالب ایمیل را درست وارد کنید مثال : mh711748@gmail.com"
+        }
+        if (!values.password) {
+            errors.password = "لطفا این قیمت را پر کنید"
+        }
+        return errors;
+    }
+
+    const validationSchema = Yup.object({
+
+        name: Yup.string().required('لطفا این قسمت را پر کنید'),
+        email: Yup.string().required('لطفا این قسمت را پر کنید').email("لطفا قالب ایمیل را درست وارد کنید مثال : mh711748@gmail.com")
+        ,password: Yup.string().required('لطفا این قسمت را پر کنید')
+
+    })
+
+
+
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        // validate
+        validationSchema
+
     })
     console.log(formik);
-    
+
 
     return (
         <div className='auth_container container-fluid d-flex justify-content-center align-items-center w-100 h-100-vh p-0'>
@@ -43,29 +64,29 @@ const Registerform = () => {
                         <div className="mb-3">
                             <label htmlFor="name" className="form-label">نام</label>
                             <input type="text" className="form-control" id="name" name='name'
-                            value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur}
+                                {...formik.getFieldProps('name')}
                             />
-                        {formik.errors.name && formik.touched.name ? <small className=' d-block text-center text-danger'>
-                            {formik.errors.name}
-                        </small> : null}
+                            {formik.errors.name && formik.touched.name ? <small className=' d-block text-center text-danger'>
+                                {formik.errors.name}
+                            </small> : null}
                         </div>
                         <div className="mb-3">
                             <label htmlFor="email" className="form-label">ایمیل</label>
                             <input type="email" className="form-control" id="email" name='email'
-                            value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur}
+                                {...formik.getFieldProps('email')}
                             />
-                        {formik.errors.email && formik.touched.email ? <small className=' d-block text-center text-danger'>
-                            {formik.errors.email}
-                        </small> : null}
+                            {formik.errors.email && formik.touched.email ? <small className=' d-block text-center text-danger'>
+                                {formik.errors.email}
+                            </small> : null}
                         </div>
                         <div className="mb-3">
                             <label htmlFor="password" className="form-label">رمز عبور</label>
                             <input type="password" className="form-control" id="password" name='password'
-                            value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur}
+                                {...formik.getFieldProps('password')}
                             />
-                        {formik.errors.password && formik.touched.password ? <small className=' d-block text-center text-danger'>
-                            {formik.errors.password}
-                        </small> : null}
+                            {formik.errors.password && formik.touched.password ? <small className=' d-block text-center text-danger'>
+                                {formik.errors.password}
+                            </small> : null}
                         </div>
                         <div className='text-center w-100'>
                             <button type="submit" className="btn btn-primary">ثبت نام</button>
